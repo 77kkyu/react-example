1a0439c97dd65e57b1af82474ef8a209d76d8747
77kkyu <77kkyu@gmail.com>
2021-09-26 14:17:54 +0900

    fix - basic setting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './tailwind.css';
+import Board from './components/board/Board';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Board />
   </React.StrictMode>,
   document.getElementById('root')
 );
